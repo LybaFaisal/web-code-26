20701136353a7bea28c9194f3a0c7ee26b62f5aa
Lyba <faisallyba06@gmail.com>
2024-01-14 01:19:33 +0500

    OperatorsGame

--- file11.js
+++ file11.js
@@ -8,12 +8,20 @@ console.log("Healthy:" + (health>50 &&  health<100));  //using + method of conca
 
 let strength= 15;
 
-console.log(`Medium Strength:  ${strength>10 && strength<20}`);  //using ${} method with (``)
+console.log(`Medium Strength:  ${strength>10 && strength<20}`);  //string interpolation Method
 
 //Example 3
 
 let weapons=  3;
 let armour =  8;
 
-console.log(`Prepared For Encounter:  ${weapons>2 && armour>5}`);
+console.log(`Prepared For Encounter:  ${weapons>2 && armour>5}`); //string interpolation Method
+
+//Example 4
+
+MagicSkill= 12;
+
+CombatSkill= 15;
+
+console.log(`Can Upgrade Skills:  ${MagicSkill>10 || CombatSkill>10}`);
 
