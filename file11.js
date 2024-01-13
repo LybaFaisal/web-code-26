841372e9939571ce6b27fea76fe011f00dffe509
Lyba <faisallyba06@gmail.com>
2024-01-14 01:14:06 +0500

    ImprovingMyLogic

--- file11.js
+++ file11.js
@@ -10,3 +10,10 @@ let strength= 15;
 
 console.log(`Medium Strength:  ${strength>10 && strength<20}`);  //using ${} method with (``)
 
+//Example 3
+
+let weapons=  3;
+let armour =  8;
+
+console.log(`Prepared For Encounter:  ${weapons>2 && armour>5}`);
+
