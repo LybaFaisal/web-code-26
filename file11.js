59a0f216c5cb29c9ca65e63d68219ba691ad2f4d
Lyba <faisallyba06@gmail.com>
2024-01-14 01:07:19 +0500

    StyleUpdate in Logic

--- file11.js
+++ file11.js
@@ -2,5 +2,11 @@
 
 let health= 75;
 
-console.log("Healthy:" + (health>50 &&  health<100))
+console.log("Healthy:" + (health>50 &&  health<100));  //using + method of concatenation
+
+//Example 2
+
+let strength= 15;
+
+console.log(`Medium Strength:  ${strength>10 && strength<20}`);  //using ${} method with (``)
 
